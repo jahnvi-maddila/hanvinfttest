be9565a99ad8afe8fcc17c9d4cec889ab1866829
Emma Nasseri <emmanasseri@Emmas-Air.midcoip.net>
2024-03-14 13:45:34 -0500

    added check for metamask

--- src/app/page.js
+++ src/app/page.js
@@ -14,10 +14,30 @@ import {
 } from "@chakra-ui/react";
 import Gallery from "./components/Gallery";
 import Minter from "./components/Minter";
+import { ethers } from "ethers";
 
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const tutorialUrl = "https://www.notion.so/YourTutorialLink"; // Replace with your actual tutorial link
+
+  const checkMetaMaskAndOpenMinter = async () => {
+    // Check if MetaMask is installed
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      // MetaMask is installed, now request access to the user's wallet
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        onOpen(); // MetaMask is connected, open the Minter
+      } catch (error) {
+        // Handle error, such as user denying access to their wallet
+        console.error("Access to MetaMask account denied:", error);
+      }
+    } else {
+      // If MetaMask is not installed, prompt the user to install it
+      alert(
+        "Please install MetaMask to use the Minting feature. \nLink to install: https://metamask.io/download/"
+      );
+    }
+  };
 
   return (
     <ChakraProvider>
@@ -25,7 +45,11 @@ export default function Home() {
         <Heading as="h1" size="2xl" marginBottom="8">
           The KU Blockchain Community Gallery
         </Heading>
-        <Button colorScheme="blue" onClick={onOpen} marginBottom="8">
+        <Button
+          colorScheme="blue"
+          onClick={checkMetaMaskAndOpenMinter}
+          marginBottom="8"
+        >
           Mint NFT
         </Button>
       </Box>
