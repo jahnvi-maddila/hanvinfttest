a510b1966b9040be0dbc5734e603876c66cab782
Emma Nasseri <emmanasseri@Emmas-Air.midcoip.net>
2024-03-14 11:29:03 -0500

    edited footer and header

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,17 @@ import {
   Heading,
   Button,
   useDisclosure,
+  Flex,
+  VStack,
+  Text,
+  Link,
 } from "@chakra-ui/react";
 import Gallery from "./components/Gallery";
 import Minter from "./components/Minter";
 
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const tutorialUrl = "https://www.notion.so/YourTutorialLink"; // Replace with your actual tutorial link
 
   return (
     <ChakraProvider>
@@ -20,14 +25,31 @@ export default function Home() {
         <Heading as="h1" size="2xl" marginBottom="8">
           The KU Blockchain Community Gallery
         </Heading>
-        {/* Button to trigger the modal */}
         <Button colorScheme="blue" onClick={onOpen} marginBottom="8">
           Mint NFT
         </Button>
       </Box>
-      <Gallery /> {/* Render the Gallery component */}
-      <Minter isOpen={isOpen} onOpen={onOpen} onClose={onClose} />{" "}
-      {/* Render the Minter component with props */}
+      <Gallery />
+      <Minter isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+
+      {/* Footer */}
+      <Flex
+        as="footer"
+        direction="column"
+        align="center"
+        justify="center"
+        marginTop="8"
+        padding="8"
+      >
+        <Text fontSize="md" marginBottom="2">
+          Made with 🔥 by the University of Kansas Blockchain Institute
+        </Text>
+        <Link href={tutorialUrl} isExternal>
+          <Button colorScheme="blue" variant="outline" size="sm">
+            View Tutorial
+          </Button>
+        </Link>
+      </Flex>
     </ChakraProvider>
   );
 }
